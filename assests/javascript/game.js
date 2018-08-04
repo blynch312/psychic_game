d3a4ceef4cfb9b601b755a8b059199c05b0392a7
Brian Lynch <brianlynch@Brians-MacBook-Pro.local>
2018-08-04 16:58:24 -0400

    update game

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,46 +1,52 @@
-var options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-var computerChoice = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var wins = 0;
+var losses = 0;
+var guessLeft = 9;
+var guessSoFar = [];
+var guessLetters = [];
+var targetLetter = null;
 
-for (var i = 0; i < options.length; i++) {
-  console.log(options[i]);
-}
 
-function reset() {
-  document.getElementById("wins").innerText = 0;
-  document.getElementById("losses").innerText = 0;
-  document.getElementById("guess-left").innerText = 9;
-  document.getElementById("guesses-so-far").innerText = "";
-}
-
-document.onkeypress = function() {
-  var userGuess = event.key;
-  var guessCount = 5;
-  var wins = 0;
-  var losses = 0;
-  var computerGuess =
-    computerChoice[Math.floor(Math.random() * computerChoice.length)];
-  var guessesText = document.getElementById("guesses-so-far").innerText;
-  document.getElementById("guesses-so-far").innerText =
-    guessesText + event.key + ", ";
-  if (options.indexOf(userGuess) > -1) {
-    if (userGuess == computerGuess) {
-      alert("You Win!");
-      wins++;
-
-      if (userGuess !== computerGuess) {
-        guessCount--;
-        options.push(userGuess);
-      }
-
-      if (guessCount === 0) {
-        reset();
-        guessCount = 5;
-        losses++;
-        guessChoice = [];
-      } else {
-        document.getElementById("guesses-so-far").innerText =
-          guessesText + event.key + ", ";
-      }
-    }
-  }
+var updateGuessSoFar = function () {
+    document.querySelector("#guessSoFar").innerHTML = guessLetters.join(", ");
 };
+
+var updateGuessLeft = function () {
+    document.querySelector("#guessLeft").innerHTML = guessLeft;
+};
+
+var updateTargetLetter = function () {
+    targetLetter = letters[Math.floor(Math.random() * letters.length)];
+};
+
+var gameReset = function () {
+    updateTargetLetter();
+    updateGuessLeft();
+    updateGuessSoFar();
+    guessLeft = 9;
+    guessLetters = [];
+};
+
+updateGuessLeft();
+updateTargetLetter();
+
+document.onkeyup = function (event) {
+    guessLeft--;
+    var letter = String.fromCharCode(event.which).toLowerCase();
+    guessLetters.push(letter);
+    updateGuessLeft();
+    updateGuessSoFar();
+
+    if (letter === targetLetter) {
+        wins++;
+        document.querySelector("#wins").innerHTML = wins;
+        gameReset();
+    }
+    if (guessLeft === 0) {
+        losses++;
+        document.querySelector("#losses").innerHTML = losses;
+        gameReset();
+    }
+
+
+}
